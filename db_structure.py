04226ae8f31a2f4299b62055f9272315c6f19725
Joel Goop <joel.goop@chalmers.se>
2014-09-27 13:34:49 +0200

    Added units and assignments to db structure

--- db_structure.py
+++ db_structure.py
@@ -1,26 +1,45 @@
 # -*- coding: utf-8 -*-
 from sqlalchemy import Column, String, Integer, ForeignKey, create_engine
+from sqlalchemy.types import Date
 from sqlalchemy.orm import relationship, backref, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import datetime
 
 Base = declarative_base()
 
 class Member(Base):
-    __tablename__ = "members"
+    __tablename__ = 'members'
     id = Column(Integer, primary_key=True)
     name = Column(String(250))
-    castvotes = relationship("CastVote", backref="member")
+    castvotes = relationship('CastVote', backref='member')
+    assignments = relationship('Assignment', backref='member')
+
+
+class Assignment(Base):
+    __tablename__ = 'assignments'
+    id = Column(Integer, primary_key=True)
+    member_id = Column(Integer, ForeignKey('members.id'))
+    unit_id = Column(Integer, ForeignKey('units.id'))
+    start_date = Column(Date)
+    end_date = Column(Date)
+
+
+class Unit(Base):
+    __tablename__ = 'units'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    assignments = relationship('Assignment', backref='unit')
 
 
 class Vote(Base):
-    __tablename__ = "votes"
+    __tablename__ = 'votes'
     id = Column(Integer, primary_key=True)
     name = Column(String(250))
-    castvotes = relationship("CastVote", backref="vote")
+    castvotes = relationship('CastVote', backref='vote')
 
 
 class CastVote(Base):
-    __tablename__ = "cast_votes"
+    __tablename__ = 'cast_votes'
     id = Column(Integer, primary_key=True)
     member_id = Column(Integer, ForeignKey('members.id'))
     vote_id = Column(Integer, ForeignKey('votes.id'))
@@ -29,19 +48,26 @@ class CastVote(Base):
 
 
 if __name__ == '__main__':
-    engine = create_engine('sqlite:///test_metadata.db')
+    engine = create_engine('postgresql+psycopg2://postgres:demokrati@localhost:5432/demokratikollen')
 
     session = sessionmaker()
     session.configure(bind=engine)
     Base.metadata.create_all(engine)
 
     s = session()
-    apa = Member(name="Arne Apa")
-    bepa = Vote(name="Bepavotering")
-    bepa_yes = CastVote(value="Jo",member=apa,vote=bepa)
+    apa = Member(name='Arne Apa')
+    bepa = Vote(name='Bepavotering')
+    bepa_yes = CastVote(value='Jo',member=apa,vote=bepa)
+    kam = Unit(name='kam')
+    start = datetime.datetime.strptime('2010-01-01', '%Y-%m-%d').date()
+    end = datetime.datetime.strptime('2011-12-12', '%Y-%m-%d').date()
+    uppdrag = Assignment(unit=kam,member=apa,start_date=start,end_date=end)
+
 
     s.add(apa)
     s.add(bepa)
     s.add(bepa_yes)
+    s.add(kam)
+    s.add(uppdrag)
 
     s.commit()
