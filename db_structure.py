d79ffbe84e01e9a294ca880a1b8a34d55cf52a92
Joel Goop <joel.goop@chalmers.se>
2014-09-28 21:08:18 +0200

    Fixing db structure, party-to-group relation

--- db_structure.py
+++ db_structure.py
@@ -12,25 +12,47 @@ Base = declarative_base()
 class Member(Base):
     __tablename__ = 'members'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
+    first_name = Column(String(250))
+    last_name = Column(String(250))
+    party_id = Column(Integer, ForeignKey('parties.id'))
     votes = relationship('Vote', backref='member')
-    assignments = relationship('Assignment', backref='member')
+    appointments = relationship('Appointment', backref='member')
+
+    def __repr__(self):
+        return '{}, {} ({})'.format(self.last_name,self.first_name,self.party.abbr)
 
 
-class Assignment(Base):
-    __tablename__ = 'assignments'
+class Appointment(Base):
+    __tablename__ = 'appointments'
     id = Column(Integer, primary_key=True)
     member_id = Column(Integer, ForeignKey('members.id'))
-    unit_id = Column(Integer, ForeignKey('units.id'))
+    group_id = Column(Integer, ForeignKey('groups.id'))
     start_date = Column(Date)
     end_date = Column(Date)
 
 
-class Unit(Base):
-    __tablename__ = 'units'
+class Group(Base):
+    __tablename__ = 'groups'
     id = Column(Integer, primary_key=True)
     name = Column(String(250))
-    assignments = relationship('Assignment', backref='unit')
+    abbr = Column(String(100))
+    appointments = relationship('Appointment', backref='group')
+    classtype = Column(String(50))
+
+    __mapper_args__ = {
+        'polymorphic_identity':'group',
+        'polymorphic_on':classtype
+    }
+
+
+class Party(Group):
+    __tablename__ = 'parties'
+    id = Column(Integer, ForeignKey('groups.id'), primary_key=True)
+    members = relationship('Member', backref='party')
+
+    __mapper_args__ = {
+        'polymorphic_identity':'party',
+    }
 
 
 class Poll(Base):
@@ -46,7 +68,7 @@ class Vote(Base):
     member_id = Column(Integer, ForeignKey('members.id'))
     poll_id = Column(Integer, ForeignKey('polls.id'))
     vote_option_id = Column(Integer, ForeignKey('vote_options.id'))
-    vote_option = relationship("VoteOption")
+    vote_option = relationship("VoteOption",uselist=False)
 
 
 class VoteOption(Base):
@@ -61,26 +83,30 @@ def create_db_structure(engine):
 
 if __name__ == '__main__':
     engine = create_engine('postgresql+psycopg2://postgres:demokrati@localhost:5432/demokratikollen')
-    session = sessionmaker()
-    session.configure(bind=engine)
     create_db_structure(engine)
 
+    session = sessionmaker()
+    session.configure(bind=engine)
     s = session()
-    apa = Member(name='Arne Apa')
+
+    sossarna = Party(name='Socialdemokraterna',abbr='S')
+
+    apa = Member(first_name='Arne',last_name='Apa',party=sossarna)
     bepa = Poll(name='Bepavotering')
     yes = VoteOption(name="yes")
     bepa_yes = Vote(member=apa,poll=bepa,vote_option=yes)
-    kam = Unit(name='kam')
     start = datetime.datetime.strptime('2010-01-01', '%Y-%m-%d').date()
     end = datetime.datetime.strptime('2011-12-12', '%Y-%m-%d').date()
-    uppdrag = Assignment(unit=kam,member=apa,start_date=start,end_date=end)
+    uppdrag = Appointment(group=sossarna,member=apa,start_date=start,end_date=end)
 
+    print apa.party.name
+    print ','.join(map(str,sossarna.members))
 
+    s.add(sossarna)
     s.add(apa)
     s.add(bepa)
     s.add(yes)
     s.add(bepa_yes)
-    s.add(kam)
     s.add(uppdrag)
 
     s.commit()
