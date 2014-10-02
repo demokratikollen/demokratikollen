ff1aaae8afb211f32316b8a566221e7d0da2981c
Johan Rohlén <johan.rohlen@gmail.com>
2014-10-02 15:32:27 +0200

    Modded windows.sh for apt-get

--- provision/windows.sh
+++ provision/windows.sh
@@ -14,23 +14,16 @@
 # export {http,https,ftp}_proxy='http://username:password@proxy-host:80'
 
 ANSIBLE_PLAYBOOK=$1
-ANSIBLE_HOSTS=$2
-TEMP_HOSTS="/tmp/ansible_hosts"
 
 if [ ! -f /vagrant/$ANSIBLE_PLAYBOOK ]; then
   echo "Cannot find Ansible playbook."
   exit 1
 fi
 
-if [ ! -f /vagrant/$ANSIBLE_HOSTS ]; then
-  echo "Cannot find Ansible hosts."
-  exit 2
-fi
-
 # Install Ansible and its dependencies if it's not installed already.
 if [ ! -f /usr/bin/ansible ]; then
   echo "Installing Ansible dependencies and Git."
-  yum install -y git python python-devel
+  apt-get install git python python-devel
   echo "Installing pip via easy_install."
   wget http://peak.telecommunity.com/dist/ez_setup.py
   python ez_setup.py && rm -f ez_setup.py
@@ -43,7 +36,6 @@ if [ ! -f /usr/bin/ansible ]; then
   pip install ansible
 fi
 
-cp /vagrant/${ANSIBLE_HOSTS} ${TEMP_HOSTS} && chmod -x ${TEMP_HOSTS}
 echo "Running Ansible provisioner defined in Vagrantfile."
-ansible-playbook /vagrant/${ANSIBLE_PLAYBOOK} --inventory-file=${TEMP_HOSTS} --extra-vars "is_windows=true" --connection=local
-rm ${TEMP_HOSTS}
+ansible-playbook /vagrant/${ANSIBLE_PLAYBOOK} --extra-vars "is_windows=true" --connection=local
+
