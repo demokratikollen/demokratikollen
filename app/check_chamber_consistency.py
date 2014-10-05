4a4a130f666313dcb19fcf5cca51281f9ef02562
Jonas Einarsson <jonas.einarsson@gmail.com>
2014-10-05 23:41:54 +0200

    consistency check in text mode instead

--- app/check_chamber_consistency.py
+++ app/check_chamber_consistency.py
@@ -11,7 +11,29 @@ engine = create_engine(utils.engine_url())
 session = sessionmaker(bind=engine)
 s = session()
 
-for chair in [1]:
-	q = s.query(ChamberAppointment).filter(ChamberAppointment.chair==1).order_by(ChamberAppointment.end_date)
-	for x in q.all():
-		print(x)
+errors = []
+for chair in range(1,350):
+	q = s.query(ChamberAppointment).filter(ChamberAppointment.chair==chair).order_by(ChamberAppointment.end_date.desc()).all()
+
+	print ('Chair #{}'.format(chair))
+	ok_until = q[0].start_date	
+	for a in q[1:]:
+		if a.status == "Ledig":
+			continue
+		if a.end_date == ok_until:
+			ok_until = a.start_date
+		elif a.end_date + dt.timedelta(days=1) == ok_until:
+			ok_until = a.start_date
+			print('WARNING: one day error at {}'.format(a.end_date))
+		else:
+			print('Break at {}, next app ends at {}'.format(ok_until,a.end_date))
+			print (a)
+			errors.append('Chair #{}: Break at {}, next app ends at {}'.format(chair,ok_until,a.end_date))
+			break
+
+		if ok_until < dt.date(2006,1,1):
+			break
+
+	print('Ok until {}'.format(ok_until))
+for err in errors:
+	print (err)
