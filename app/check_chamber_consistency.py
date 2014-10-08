6f06c3c3b089b58ef30642bc315f40fe32f5545c
Jonas Einarsson <jonas.einarsson@gmail.com>
2014-10-08 23:57:08 +0200

    Started on algorithm: 'timeline' for a chair
    All broken atm, algorithm not trivial if one cannot
    assume anything about data. Output will be a
    partition of the timeline where each interval has
    0..n members. Anything but 1 member is a data error.

--- app/check_chamber_consistency.py
+++ app/check_chamber_consistency.py
@@ -4,36 +4,45 @@ from sqlalchemy.orm import sessionmaker, aliased
 from sqlalchemy.sql.expression import literal
 from itertools import combinations
 import utils
-
 import datetime as dt
 engine = create_engine(utils.engine_url())
 
 session = sessionmaker(bind=engine)
 s = session()
 
-errors = []
 for chair in range(1,350):
-	q = s.query(ChamberAppointment).filter(ChamberAppointment.chair==chair).order_by(ChamberAppointment.end_date.desc()).all()
+	q = s.query(ChamberAppointment) \
+		.filter(ChamberAppointment.chair==chair)\
+		.filter(ChamberAppointment.status != 'Ledig')\
+		.order_by(ChamberAppointment.end_date.desc()).all()
 
-	print ('Chair #{}'.format(chair))
-	ok_until = q[0].start_date	
-	for a in q[1:]:
-		if a.status == "Ledig":
-			continue
-		if a.end_date == ok_until:
-			ok_until = a.start_date
-		elif a.end_date + dt.timedelta(days=1) == ok_until:
-			ok_until = a.start_date
-			print('WARNING: one day error at {}'.format(a.end_date))
+	starts = [(1,a,a.start_date) for a in q]
+	ends = [(-1,a,a.end_date) for a in q]
+	all_dates = starts+ends
+	all_dates.sort(key=lambda t:t[2])
+
+
+	one_day = dt.timedelta(days=1)
+	out_intervals = []
+	current_members = []
+	last_date = dt.date(1900,1,1)
+
+	for (delta, app, date) in all_dates:
+		
+		if delta == 1:
+			if date != last_date:
+				out_intervals.append( (last_date, date - one_day, current_members) )
+			current_members.append(app.member.id)
 		else:
-			print('Break at {}, next app ends at {}'.format(ok_until,a.end_date))
-			print (a)
-			errors.append('Chair #{}: Break at {}, next app ends at {}'.format(chair,ok_until,a.end_date))
-			break
+			if date != last_date:
+				out_intervals.append( (last_date, date, current_members) )
+			current_members.remove(app.member.id)
 
-		if ok_until < dt.date(2006,1,1):
-			break
+		last_date = date
 
-	print('Ok until {}'.format(ok_until))
-for err in errors:
-	print (err)
+	print('Chair #{}'.format(chair))
+	for i in out_intervals[1:]:
+		if len(i[2]) == 0:
+			print('{} - {}: Chair empty.'.format(i[0],i[1]))
+		if len(i[2]) > 1:
+			print('{} - {}: Multiple: {}'.format(i[0],i[1],i[2]))
