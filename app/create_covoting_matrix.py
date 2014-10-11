1a1774278c7f4531891dccb2b85e553dd049e78b
Johan Rohlén <johan.rohlen@gmail.com>
2014-10-11 21:29:30 +0200

    Changed to work with new mongodb datastore

--- app/create_covoting_matrix.py
+++ app/create_covoting_matrix.py
@@ -11,6 +11,13 @@ import colorsys
 import math
 from utils import *
 
+def row2dict(row):
+    d = {}
+    for column in row.__table__.columns:
+        d[column.name] = str(getattr(row, column.name))
+
+    return d
+
 def create_covoting_matrix(session, member_map):
     n_members = len(member_map)
 
@@ -47,40 +54,33 @@ def get_active_members(session, vote_cutoff=1):
                                             .order_by(Party.abbr)
     for i, member in enumerate(q.all()):
         if member[1] > vote_cutoff:
-            members.append({ "member": member[0], "party_abbr": member[2]})
+            members.append({ "member": row2dict(member[0]), "party_abbr": member[2]})
     return members
 
-def get_data(session,load=True, vote_cutoff=1):
+def get_data(session, datastore,load=True, vote_cutoff=1):
     
     if load:
         #load matrix and membermap
-        with open('data/covoting_matrix.dat', 'rb') as f:
-            covoting_matrix = pickle.load(f)
-        with open('data/member_map.dat', 'rb') as f:
-            member_map = pickle.load(f)
-        with open('data/members.dat', 'rb') as f:
-            members = pickle.load(f)
+        covoting_matrix = datastore.get_object('covoting_matrix')
+        member_map = datastore.get_object('member_map')
+        members = datastore.get_object('members')
     else:
         print("Getting member list")
         members = get_active_members(s,50)
+        datastore.store_object(members, 'members')
         n_members = len(members)
         print("Number of members: " + str(n_members))
 
         #create member map
         member_map = {}
         for i, member_dict in enumerate(members):
-            member_map[member_dict['member'].id] = i
+            print(member_dict['member'])
+            member_map[member_dict['member']['id']] = i
+        datastore.store_object(member_map, 'member_map')
 
         covoting_matrix = create_covoting_matrix(session, member_map)
-
-        #save matrix and membermap
-        with open('data/covoting_matrix.dat', 'wb') as f:
-            pickle.dump(covoting_matrix, f)
-        with open('data/member_map.dat', 'wb') as f:
-            pickle.dump(member_map, f)
-        with open('data/members.dat', 'wb') as f:
-            pickle.dump(members,f)
-            
+        datastore.store_object(covoting_matrix, 'covoting_matrix')
+                    
     return members, member_map, covoting_matrix
 
 
@@ -91,14 +91,15 @@ session = sessionmaker()
 session.configure(bind=engine)
 s = session()
 
-mongodb = MongoUtils.mongodb_connection()
+datastore = MongoDBDatastore()
 
 if MiscUtils.yes_or_no("Do you want to get data from the database again?"):
-    members, member_map, covoting_matrix = get_data(s,False)
+    members, member_map, covoting_matrix = get_data(s,datastore,False)
 else:
-    members, member_map, covoting_matrix = get_data(s)
+    members, member_map, covoting_matrix = get_data(s,datastore)
 
-n_members = len(members)
+for member in members:
+    print(member['member'])
 
 abbr_color = {'M': colorsys.rgb_to_hls(32/256,76/256,212/256), \
                           'C': colorsys.rgb_to_hls(0/256,153/256,68/256), \
@@ -108,37 +109,5 @@ abbr_color = {'M': colorsys.rgb_to_hls(32/256,76/256,212/256), \
                           'S': colorsys.rgb_to_hls(247/256,17/256,39/256), \
                           'SD': colorsys.rgb_to_hls(220/256,220/256,74/256), \
                           'V': colorsys.rgb_to_hls(182/256,0/256,9/256) }
-
-
-img_together = Image.new('RGB', (n_members,n_members), "black")
-pixels_together = img_together.load()
-img_different = Image.new('RGB', (n_members,n_members), "black")
-pixels_different = img_different.load()
-
-for i,row in enumerate(covoting_matrix):
-    for j,column in enumerate(row):
-        #print(str(column['vote_equal']) + " " + str(column['vote_different']))
-        if column['vote_equal']+column['vote_different'] > 0:
-            party_abbr = members[i]['party_abbr']
-                
-            if party_abbr in abbr_color:
-                current_abbr_color = abbr_color[party_abbr]
-            else:
-                current_abbr_color = colorsys.rgb_to_hls(0,0,0)
-            
-            fraction_together = column['vote_equal']/(column['vote_equal']+column['vote_different'])
-            
-            log_fraction_together = 0.01*10**(1+fraction_together)
-            
-            r, g, b = colorsys.hls_to_rgb(current_abbr_color[0],log_fraction_together,current_abbr_color[2])
-            pixels_together[i,j] = (int(r*255),int(g*255),int(b*255))
-            r, g, b = colorsys.hls_to_rgb(current_abbr_color[0],1 - log_fraction_together,current_abbr_color[2])
-            pixels_different[i,j] = (int(r*255),int(g*255),int(b*255))
-            
-            
-img_together = img_together.rotate(90)
-img_together.save('figs/covoting-together.png')            
-
-img_different = img_different.rotate(90)
-img_different.save('figs/covoting-different.png')            
+                                  
 
