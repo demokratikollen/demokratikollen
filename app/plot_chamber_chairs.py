f07937ce9dcecb974a22990c54ba87ab962c87ea
Jonas Einarsson <jonas.einarsson@gmail.com>
2014-10-15 00:39:29 +0200

    first reasonable plot of chair occupancy

--- app/plot_chamber_chairs.py
+++ app/plot_chamber_chairs.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, func, distinct
 from sqlalchemy.orm import sessionmaker, aliased
 from sqlalchemy.sql.expression import literal, distinct
 from itertools import combinations
-import utils
+from utils import PostgresUtils
 import datetime as dt
 import codecs
 import numpy as np
@@ -13,47 +13,69 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-def plot_intervals(members, chair):
-
-
-    for member in members:
-        for app in (app for app in member.appointments if app.chair):
-            print(app)
-
+from collections import OrderedDict
+import os
 
 def main():
     
+    fig_dir = 'chamber_chair_figs'
 
-    engine = create_engine(utils.engine_url())
+    if not os.path.exists(fig_dir):
+        os.makedirs(fig_dir)
 
+    engine = create_engine(PostgresUtils.engine_url()) 
     session = sessionmaker(bind=engine)
     s = session()
 
 
-    chair = 7
-    sq = s.query(ChamberAppointment) \
-        .filter(ChamberAppointment.chair==chair) \
-        .order_by(ChamberAppointment.end_date.asc()).subquery()
-    
-    # distinct members which have been on this chair
-    members = s.query(Member).filter(Member.id.in_( s.query(distinct(sq.c.member_id)) ))
+    for chair in range(1,350):
+        q = s.query(ChamberAppointment).join(Member) \
+            .filter(ChamberAppointment.chair==chair) \
+            .order_by(ChamberAppointment.end_date.desc())
+        
+     
+     
+        rows = OrderedDict()
+        for app in q:
+            if not app.member.id in rows:
+                rows[app.member.id] = [app]
+            else:
+                rows[app.member.id].append(app)
 
-    fig = plt.figure(figsize=(10,8))
-    ax = fig.add_subplot(111)    
- 
-    y = 0
-    for m in members: 
-        apps = s.query(ChamberAppointment) \
-                .filter(ChamberAppointment.chair==chair) \
-                .filter(ChamberAppointment.member_id == m.id) \
-                .order_by(ChamberAppointment.end_date.asc())
-        for app in apps:
 
-            ax.barh(y,mdates.date2num(app.end_date)-mdates.date2num(app.start_date),left=app.start_date)
+        hatches = {'Tjänstgörande':'','Ledig':'x'}
+        colors = {'Riksdagsledamot': '#ff0000', 'Ersättare':'#aaaa22'}
 
-        y += 1
-    ax.xaxis_date()
-    fig.savefig('test.png')
+
+        fig = plt.figure(figsize=(100,len(rows)))
+        ax = fig.add_subplot(111)    
+
+        y = 0
+        yticklabelss = []
+        yticks = []
+        for (member_id, apps) in rows.items():
+
+            member = s.query(Member).filter(Member.id == member_id).one()
+
+            yticks.append(y+0.5)
+            yticklabelss.append(repr(member))
+
+            for app in apps:
+                ax.barh(y,mdates.date2num(app.end_date)-mdates.date2num(app.start_date),left=app.start_date,
+                    hatch=hatches[app.status],
+                    color=colors[app.role],
+                    linewidth=0)
+
+            y += 1
+
+
+        ax.set_yticks(yticks)
+        ax.set_yticklabels(yticklabelss)
+        ax.xaxis_date()
+        plt.tight_layout()
+
+        fig.savefig('{}/{:03d}.png'.format(fig_dir,chair))
+        
 
 if __name__ == '__main__':
         main()  
