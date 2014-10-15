a68cd387395317751e49e7a1a0752750929b7515
Joel Goop <joel.goop@chalmers.se>
2014-10-15 21:44:37 +0200

    Created JSON route

--- app/www/app/mod_members/controllers.py
+++ app/www/app/mod_members/controllers.py
@@ -1,6 +1,7 @@
 # Import flask dependencies
 from flask import Blueprint, request, render_template, \
-                  flash, g, session, redirect, url_for
+                  flash, g, session, redirect, url_for, \
+                  json
 
 # Import the database object from the main app module
 from app import db
@@ -13,3 +14,16 @@ mod_members = Blueprint('members', __name__, url_prefix='/members')
 def member():
     return render_template("/members/members.html",
                             header_members_class="active")
+
+@mod_members.route('/<int:member_id>.<string:format>', methods=['GET'])
+def get_member(member_id,format):
+    VALID_FORMATS = ['json']
+    if not format in VALID_FORMATS:
+        raise ValueError('Format not recognized.')
+    member = {
+                "name": "Test T. Testsson",
+                "attendance": [0.8,0.7,0.6,0.8,1.0],
+                "id": member_id
+            }
+    return json.jsonify(member)
+
