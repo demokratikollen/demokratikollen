d48e3923eb714132a882113261b980e09576ce91
Joel Goop <joel.goop@chalmers.se>
2014-10-15 23:17:07 +0200

    Added an absence view to show absence of members
    Created a JSON API for members.

--- app/www/app/mod_members/controllers.py
+++ app/www/app/mod_members/controllers.py
@@ -4,7 +4,7 @@ from flask import Blueprint, request, render_template, \
                   json
 
 # Import the database object from the main app module
-from app import db
+from app import db, Member
 
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 mod_members = Blueprint('members', __name__, url_prefix='/members')
@@ -15,15 +15,23 @@ def member():
     return render_template("/members/members.html",
                             header_members_class="active")
 
+# Set the route and accepted methods
+@mod_members.route('/<int:member_id>/absence', methods=['GET'])
+def absence_graph(member_id):
+    return render_template("/members/absence.html",member_id=member_id)
+
 @mod_members.route('/<int:member_id>.<string:format>', methods=['GET'])
 def get_member(member_id,format):
     VALID_FORMATS = ['json']
     if not format in VALID_FORMATS:
         raise ValueError('Format not recognized.')
+
+    m = db.session.query(Member).filter_by(id=member_id).first()
+
+
     member = {
-                "name": "Test T. Testsson",
-                "attendance": [0.8,0.7,0.6,0.8,1.0],
-                "id": member_id
+                "first_name": m.first_name,
+                "last_name": m.last_name
             }
     return json.jsonify(member)
 
