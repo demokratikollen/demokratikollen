c80682cc756e5d04cd4bfc1633ee02b4316f1ca3
Jonas Einarsson <jonas.einarsson@gmail.com>
2014-10-18 18:53:52 +0200

    pollfreqs datamined

--- app/fig_poll_freq.py
+++ app/fig_poll_freq.py
@@ -27,12 +27,14 @@ def poll_freq():
 
 
     weekday = {k: 0 for k in range(1,8)}
+    week = {k: 0 for k in range(1,54)}
     month = {k: 0 for k in range(1,13)}
     day = {k: 0 for k in range(1,32)}
     exact = dict()
 
     for poll in s.query(Poll).all():
         weekday[poll.date.isoweekday()] += 1
+        week[poll.date.isocalendar()[1]] += 1
         month[poll.date.month] += 1
         day[poll.date.day] += 1
 
@@ -46,17 +48,42 @@ def poll_freq():
     datastore = MongoDBDatastore() 
     datastore.store_string(json.dumps(weekday),'poll_frequency_weekday')
     datastore.store_string(json.dumps(month),'poll_frequency_month')
+    datastore.store_string(json.dumps(month),'poll_frequency_week')
     datastore.store_string(json.dumps(day),'poll_frequency_day')
     datastore.store_string(json.dumps(exact),'poll_frequency_exact')
 
 
     fig = plt.figure(0, figsize=(10,10))
     ax = fig.add_subplot(111)
-
-    ax.hist(weekday)
-
+    xs = np.arange(1,8)-0.4
+    ax.bar(xs, [weekday[i] for i in range(1,8)])
+    ax.set_xticks(np.arange(1,8))
     fig.savefig('pollfreq_weekday.png')
+    plt.clf()
 
+    fig = plt.figure(0, figsize=(10,10))
+    ax = fig.add_subplot(111)
+    xs = np.arange(1,13)-0.4
+    ax.bar(xs, [month[i] for i in range(1,13)])
+    ax.set_xticks(np.arange(1,13))
+    fig.savefig('pollfreq_month.png')
+    plt.clf()
+
+    fig = plt.figure(0, figsize=(20,10))
+    ax = fig.add_subplot(111)
+    xs = np.arange(1,54)-0.4
+    ax.bar(xs, [week[i] for i in range(1,54)])
+    ax.set_xticks(np.arange(1,54,5))
+    fig.savefig('pollfreq_week.png')
+    plt.clf()
+
+    fig = plt.figure(0, figsize=(10,10))
+    ax = fig.add_subplot(111)
+    xs = np.arange(1,32)-0.4
+    ax.bar(xs, [day[i] for i in range(1,32)])
+    ax.set_xticks(np.arange(1,32))
+    fig.savefig('pollfreq_day.png')
+    plt.clf()
 
 def main():
     poll_freq()
