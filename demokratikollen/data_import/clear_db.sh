f957439c6fc0e97d750ea1fec6ae4643048f4096
Rasmus Einarsson <rasmus.einarsson@gmail.com>
2014-10-19 23:14:35 +0200

    Remove obsolete bash scripts

--- demokratikollen/data_import/clear_db.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-psql -U vagrant -d riksdagen -t -c "select 'drop table \"' || tablename || '\" cascade;' from pg_tables where schemaname = 'public'" | psql -U vagrant -d riksdagen
--- demokratikollen/data_import/init_db.sh
+++
@@ -1,6 +0,0 @@
-#/bin/sh
-
-psql -U vagrant -d riksdagen -q -f create_tables.sql
-psql -U vagrant -d riksdagen -q -f psql_person.sql
-psql -U vagrant -d riksdagen -q -f psql_votering-201213.sql
-psql -U vagrant -d riksdagen -q -f psql_bet-2010-2013.sql
