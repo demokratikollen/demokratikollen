06c2f8e75407986afd43c1ac5f1c8cf0db3c12ae
Johan Rohlén <johan.rohlen@gmail.com>
2014-10-31 22:16:02 +0100

    typos and ctrl+c ctrl+v mistakes

--- deployment.sh
+++ deployment.sh
@@ -7,24 +7,24 @@ mkdir docker/postgres
 mkdir docker/mongo
 
 #copy files
-cp -r src/dockerfiles/webapp/ docker/app
-cp -r src/dockerfiles/postgres/ docker/postgres
-cp -r src/dockerfiles/mongo.dockerfile/ docker/mongo
+cp -r src/dockerfiles/webapp docker/app
+cp -r src/dockerfiles/postgres docker/postgres
+cp -r src/dockerfiles/mongo docker/mongo
 
 cp -r src/demokratikollen/ docker/app/
 
 
 #Build the postgres image if it does not exist.
-if [-f /docker/imids/postgres]; then
+if [ ! -f /docker/imids/postgres ]; then
     docker build -t demokratikollen/postgres docker/postgres > /home/wercker/docker/imids
 fi
 
 #If the container isn't alreay running, start it
-if [-f /docker/coids/postgres]; then
-    docker run -d --name postgres demokratikollen/postgres
+if [ ! -f /docker/coids/postgres ]; then
+    docker run -d --name postgres demokratikollen/postgres > /home/wercker/docker/coids
 fi
 
 #remove files.
-rm -rf src/dockerfiles/webapp/ docker/app
-rm -rf src/dockerfiles/postgres/ docker/postgres
-rm -rf src/dockerfiles/mongo.dockerfile/ docker/mongo
+rm -rf docker/app
+rm -rf docker/postgres
+rm -rf docker/mongo
