2f7405e7c7e1c9f65197f62e7f164532b44bf809
Johan Rohlén <johan.rohlen@gmail.com>
2014-10-31 23:00:38 +0100

    typo

--- dockerfiles/postgres/setup-dbs.sh
+++ dockerfiles/postgres/setup-dbs.sh
@@ -14,4 +14,4 @@ gosu postgres postgres --single <<- EOSQL
    GRANT ALL PRIVILEGES ON DATABASE demokratikollen to demokratikollen;
 EOSQL
 echo ""
-echo "******DOCKER DATABASE CREATED******
+echo "******DOCKER DATABASE CREATED******"
