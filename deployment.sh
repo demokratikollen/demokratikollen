dc98f5b933925eed2cb04368947abec2175d49f5
Johan Rohlén <johan.rohlen@gmail.com>
2014-11-01 10:58:18 +0100

    Changed provisioning to use sudo only on docker commands

--- deployment.sh
+++ deployment.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-#copy files
-cp -r src/dockerfiles/webapp docker
-cp -r src/dockerfiles/postgres docker
-cp -r src/dockerfiles/mongo docker
+mkdir -p docker
 
-cp -r src/demokratikollen/* docker/webapp
+#copy files
+cp -r src/dockerfiles/webapp docker/
+cp -r src/dockerfiles/postgres docker/
+cp -r src/dockerfiles/mongo docker/
+
+cp -r src/demokratikollen/* docker/webapp/
 
 
 #Get the postgres image id, if it does not exist, create it
-postgres_image_id=`docker images | sed -nr 's/demokratikollen\/postgres\s*latest\s*([a-z0-9]*).*/\1/p'`
+postgres_image_id=`sudo docker images | sed -nr 's/demokratikollen\/postgres\s*latest\s*([a-z0-9]*).*/\1/p'`
 
 if [ -z $postgres_image_id ]; then
-    postgres_image_id=`docker build -t demokratikollen/postgres docker/postgres`
+    postgres_image_id=`sudo docker build -t demokratikollen/postgres docker/postgres`
 fi
 
 #Get the posgres container id, if it does not exist create it
 postgres_container_id=`sudo docker ps | sed -nr 's/([a-z0-9]*)\s*demokratikollen\/postgres.*/\1/p'`
 
-if [ -z -f $postgres_container_id ]; then
-    postgres_container_id=`docker run -d --name postgres demokratikollen/postgres`
+if [ -z $postgres_container_id ]; then
+    postgres_container_id=`sudo docker run -d --name postgres demokratikollen/postgres`
 fi
 
 #Get the webapp image id. Build it if it does not exist
-webapp_image_id=`docker images | sed -nr 's/demokratikollen\/webapp\s*latest\s*([a-z0-9]*).*/\1/p'`
+webapp_image_id=`sudo docker images | sed -nr 's/demokratikollen\/webapp\s*latest\s*([a-z0-9]*).*/\1/p'`
 
 if [ -z $webapp_image_id ]; then
-	webapp_image_id=`docker build -t demokratikollen/webapp docker/webapp`
+	webapp_image_id=`sudo docker build -t demokratikollen/webapp docker/webapp`
 	#build environment variables needed by the webapp
 	#envs="DATABASE_URL=postgresql://demokratikollen@localhost:"
 	#link the webapp to the postgresql database.
