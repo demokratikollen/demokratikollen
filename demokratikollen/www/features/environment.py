cac11b0bd9ab4b3b8e2419037c0a21f301c23216
Johan Rohlén <johan.rohlen@gmail.com>
2014-11-08 19:00:01 +0000

    Added testing steps to wercker.yml

--- demokratikollen/www/features/environment.py
+++ demokratikollen/www/features/environment.py
@@ -6,7 +6,7 @@ import shlex
 
 def before_all(context):
    print("Starting Webserver and Webdriver")
-   context.server = subprocess.Popen(shlex.split("gunicorn --debug -b 127.0.0.1:5555 app:app"),stdout=subprocess.PIPE)
+   context.server = subprocess.Popen(shlex.split("gunicorn --debug -b 127.0.0.1:5555 demokratikollen.www.app:app"),stdout=subprocess.PIPE)
    context.driver = webdriver.PhantomJS()
    context.app = app
    #fulhack
