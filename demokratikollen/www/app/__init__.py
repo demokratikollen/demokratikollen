ff01148138da6442fe5a842674bb40d6e8428462
Johan Rohlén <johan.rohlen@gmail.com>
2014-11-09 10:35:44 +0000

    Rewritten with methods for each setup step

--- demokratikollen/www/app/__init__.py
+++ demokratikollen/www/app/__init__.py
@@ -10,26 +10,49 @@ from demokratikollen.www.app.helpers.cache import cache
 #Import the orm.
 from demokratikollen.core.db_structure import *
 
-# Define the WSGI application object
-app = Flask(__name__, static_url_path='/')
+import os
 
-# Configurations
-app.config.from_object('demokratikollen.www.config')
+def create_app(testing=False):
+    # Define the WSGI application object
+    app = Flask(__name__, static_url_path='/')
+    # Configurations
+    app.config.from_object('demokratikollen.www.config')
 
-#init the cache
-cache.init_app(app)
+    if testing:
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['TEST_DATABASE_URL']
 
-# Define the database object which is imported
-# by modules and controllers
-db = SQLAlchemy(app)
+    return app
 
-# 404 error handling
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
+def create_db(app):
+    return SQLAlchemy(app)
 
-# Import a module / component using its blueprint handler variable (mod_auth)
-from .mod_static.controllers import mod_static as static_module
+def setup_app(app):    
+    #init the cache
+    cache.init_app(app)
 
-# Register blueprint(s)
-app.register_blueprint(static_module)
+    # 404 error handling
+    @app.errorhandler(404)
+    def not_found(error):
+        return render_template('404.html'), 404
+
+    # Import a module / component using its blueprint handler variable
+    from .mod_static.controllers import mod_static as static_module
+
+    # Register blueprint(s)
+    app.register_blueprint(static_module)
+
+if 'TESTING' in os.environ:
+    testing = True
+else:
+    testing = False
+
+app = create_app(testing=testing)
+db = create_db(app)
+setup_app(app)
+
+
+
+
+
+
