559ab26b91302859329f6acf1225f4bab30a7c32
Johan Rohlén <johan.rohlen@gmail.com>
2014-11-10 20:06:08 +0000

    Changed initialization of webapp to help with testing

--- demokratikollen/www/app/__init__.py
+++ demokratikollen/www/app/__init__.py
@@ -1,36 +1,31 @@
 # Import flask and template operators
 from flask import Flask, render_template
 
-# Import extensions
-from flask.ext.sqlalchemy import SQLAlchemy
-
-#import the cache helper
+#import helpers
 from demokratikollen.www.app.helpers.cache import cache
-
-#Import the orm.
-from demokratikollen.core.db_structure import *
+from demokratikollen.www.app.helpers.db import db
 
 import os
 
+#App Factory to facilitate testing.
 def create_app(testing=False):
-    # Define the WSGI application object
     app = Flask(__name__, static_url_path='/')
-    # Configurations
-    app.config.from_object('demokratikollen.www.config')
 
+    #Load the basic config.
+    app.config.from_object('demokratikollen.www.config')
+    
+    #If we require testing. change some configs.
     if testing:
         print("Running Test env.")
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['TEST_DATABASE_URL']
+        cache.config['CACHE_TYPE'] = 'null'
 
-    return app
-
-def create_db(app):
-    return SQLAlchemy(app)
-
-def setup_app(app):    
     #init the cache
     cache.init_app(app)
+
+    #inti the db
+    db.init_app(app)
 
     # 404 error handling
     @app.errorhandler(404)
@@ -43,14 +38,9 @@ def setup_app(app):
     # Register blueprint(s)
     app.register_blueprint(static_module)
 
-if 'TESTING' in os.environ:
-    testing = True
-else:
-    testing = False
+    return app
 
-app = create_app(testing=testing)
-db = create_db(app)
-setup_app(app)
+
 
 
 
