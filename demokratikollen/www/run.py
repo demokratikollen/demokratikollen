0d305e23f15b45b953563a4e5c97d26c2c9c03cd
Johan Rohlén <johan.rohlen@gmail.com>
2014-11-10 20:06:41 +0000

    Changed to work with new initialization of app

--- demokratikollen/www/run.py
+++ demokratikollen/www/run.py
@@ -1,3 +1,5 @@
-# Run a test server.
-from app import app
+# Run a development server.
+from demokratikollen.www.app import create_app
+
+app = create_app()
 app.run(host='0.0.0.0', port=8000, debug=True)
