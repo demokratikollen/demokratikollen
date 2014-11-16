d35952542205d2c8836e66c158cbe0b30e695cd0
Johan Rohlén <johan.rohlen@gmail.com>
2014-11-16 22:54:33 +0100

    Added delay to behave inits

--- demokratikollen/www/features/environment.py
+++ demokratikollen/www/features/environment.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 import subprocess
 import os
 import shlex
+import time
 
 
 def before_all(context):
    print("Starting Webserver and Webdriver")
    context.server = subprocess.Popen(shlex.split("gunicorn --debug -b 127.0.0.1:5555 demokratikollen.www.gunicorn_testing:app"),stdout=subprocess.PIPE)
+   time.sleep(2)
    context.driver = webdriver.PhantomJS()
    #fulhack
    
