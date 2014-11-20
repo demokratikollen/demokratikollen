47099fb8dc2c94dafb91995f99bc6ae23731751b
Johan Rohlén <johan.rohlen@gmail.com>
2014-11-20 17:12:13 +0100

    Finalized json generation of gender data

--- demokratikollen/www/app/routes/data.py
+++ demokratikollen/www/app/routes/data.py
@@ -1,10 +1,13 @@
-from datetime import date
+import datetime
 
 # Import flask dependencies
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 #import the relevant controllers
 from demokratikollen.www.app.controllers import data
+from demokratikollen.www.app.helpers.db import db
+from demokratikollen.www.app.helpers.cache import cache
+from demokratikollen.core.db_structure import Party
 
 # Define the blueprint: 'auth', set its url prefix: app.url/auth
 data_blueprint = Blueprint('data', __name__, url_prefix='/data')
@@ -12,5 +15,29 @@ data_blueprint = Blueprint('data', __name__, url_prefix='/data')
 # Set the route and accepted methods
 @data_blueprint.route('/gender.json', methods=['GET'])
 def gender_json():
-    a_date = request.args.get('date', date.today().isoformat())
-    return data.gender_json(date=a_date)
+    str_date = request.args.get('date', '')
+    party = request.args.get('party','')
+    
+    #Check if the party exists
+    if party and party not in get_parties():
+        msg = "Partiförkortningen existerar ej. Möjliga är: <br>"
+        for p in get_parties():
+            msg = msg + p + "<br>"
+        return msg, 400
+
+    # Check if the date supplied is ok.
+    if not str_date:
+        date = datetime.date.today()
+    else:
+        try:
+            date = datetime.datetime.strptime(str_date, '%Y-%m-%d').date()
+        except ValueError:
+            return "Felaktig datumparameter. Formatet är: ÅÅ-MM-DD", 400
+
+    json = data.gender_json(date=date,party=party)
+
+    return jsonify(json)
+
+@cache.cached(3600*24)
+def get_parties():
+    return [r[0] for r in db.session.query(Party.abbr).all()]
--- demokratikollen/www/specs/routes_test.py
+++ demokratikollen/www/specs/routes_test.py
@@ -1,31 +1,50 @@
 import os
+import datetime
 from flask.ext.testing import TestCase
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, Mock, patch
 
 from demokratikollen.www.app import create_app
+from demokratikollen.www.app.routes import data
 
-class TestRoutes(TestCase):
 
+class TestRoutesDataGender(TestCase):
     def create_app(self):
         return create_app(testing=True)
 
-    #Gender routing:
-    def test_gender_json_200(self):
+    def setUp(self):
+        data.data.gender_json = Mock(return_value=dict(data=[{'test': 1}]))
+        data.get_parties = Mock(return_value=['M'])
+
+    def test_gender_json(self):
         response = self.client.get('/data/gender.json')
         self.assert_200(response)
 
-    def test_gender_json_params(self):
-    	from demokratikollen.www.app.controllers import data
-    	from datetime import date as d
-    	data.gender_json = MagicMock(return_value="Hello World")
-    	date = '2014-01-01'
+    def test_gender_json_date(self):
+        str_date = '2014-01-01'
+        #test that it works with a param
+        self.client.get('/data/gender.json?date=' + str_date)
+        date = datetime.datetime.strptime('2014-01-01', '%Y-%m-%d').date()
+        data.data.gender_json.assert_called_with(date=date,party='')
 
-    	#test that it works with a param
-    	self.client.get('/data/gender.json?date=' + date)
-    	data.gender_json.assert_called_with(date=date)
+    def test_gender_json_no_date(self):
+        self.client.get('/data/gender.json')
+        data.data.gender_json.assert_called_with(date=datetime.date.today(),party='')
 
-    	#Test that it works without.
-    	self.client.get('/data/gender.json')
-    	data.gender_json.assert_called_with(date=d.today().isoformat())
+    def test_gender_json_bad_date(self):
+        response = self.client.get('/data/gender.json?date=not_a_date')
+        self.assert_400(response)
 
+    def test_gender_json_party(self):
+        response = self.client.get('data/gender.json?party=M')
+
+        data.data.gender_json.assert_called_with(date=datetime.date.today(),party='M')
+
+    def test_gender_json_no_party(self):
+        response = self.client.get('data/gender.json?party=')
+
+        data.data.gender_json.assert_called_with(date=datetime.date.today(),party='')
+
+    def test_gender_json_bad_party(self):
+        response = self.client.get('data/gender.json?party=not-a-pary-abbr')
+        self.assert_400(response)
 
