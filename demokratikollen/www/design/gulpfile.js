697434b28ee5e6e458edc0e8ff90f7707287511f
Joel Goop <joel.goop@chalmers.se>
2014-11-24 21:18:59 +0100

    Switch to gulp and make all static dependencies local
     - Grunt has been exchanged to gulp
     - JS and CSS dependencies are specified in www/design/gulpfile.js
     - JS and CSS dependencies are concatenated and minified
     - Other files, e.g. fonts and images, are copied
     - layout.html has been adapted to new styles/scripts
     - Old static files have been removed

--- demokratikollen/www/design/gulpfile.js
+++ demokratikollen/www/design/gulpfile.js
@@ -1,44 +1,110 @@
+/* ========================================================================
+ * Setup
+ * ======================================================================== */
 var gulp = require('gulp'),
-    conc = require('gulp-concat'),
+    concatenate = require('gulp-concat'),
     uglify = require('gulp-uglify'),
+    sourcemaps = require('gulp-sourcemaps'),
+    minifyCss = require('gulp-minify-css'),
     rename = require('gulp-rename'),
     sass = require('gulp-ruby-sass');
 
-var work_dir = '/home/vagrant/demokratikollen/www/design/';
-var dist_dir = '/home/vagrant/demokratikollen/www/app/static/';
+var work_dir = '/home/vagrant/demokratikollen/www/design/',
+    dist_dir = '/home/vagrant/demokratikollen/www/app/static/';
 
-var bs_assets_dir = 'bower_components/bootstrap-sass-official/assets/';
-var bs_styles_dir = work_dir+bs_assets_dir+'stylesheets/'
-var bs_scripts_dir = work_dir+bs_assets_dir+'javascripts/'
+/* ========================================================================
+ * Dependencies
+ * ======================================================================== */
+var bs_assets_dir = 'bower_components/bootstrap-sass-official/assets/',
+    bs_styles_dir = work_dir+bs_assets_dir+'stylesheets/',
+    bs_scripts_dir = work_dir+bs_assets_dir+'javascripts/';
 
-var bs_js_deps = [bs_scripts_dir+'bootstrap/collapse.js']
-// var bs_js_deps = [bs_scripts_dir+'bootstrap.js']
+var jqueryUiDir = 'bower_components/jqueryui/',
+    jqueryUiThemeDir = jqueryUiDir+'themes/smoothness/',
+    jqueryUiSliderPipsDir = 'bower_components/jquery-ui-slider-pips/dist/';
 
-var sass_files = work_dir+'sass/*.scss'
-var js_files = work_dir+'javascripts/*.js'
-var css_dist = dist_dir+'css'
-var js_dist = dist_dir+'js'
+var js_deps = [
+    // Main libraries
+    'bower_components/jquery/dist/jquery.js',
+    'bower_components/d3/d3.js',
+    // jQueryUI components
+    jqueryUiDir+'ui/core.js',
+    jqueryUiDir+'ui/widget.js',
+    jqueryUiDir+'ui/mouse.js',
+    jqueryUiDir+'ui/slider.js',
+    jqueryUiSliderPipsDir+'jquery-ui-slider-pips.js',
+    // Bootstrap plugins
+    bs_scripts_dir+'bootstrap/transition.js',
+    bs_scripts_dir+'bootstrap/collapse.js'
+];
 
+var style_deps = [
+    jqueryUiThemeDir+'jquery-ui.css',
+    jqueryUiSliderPipsDir+'jquery-ui-slider-pips.css'
+];
+
+/* ========================================================================
+ * Paths for collection/compilation and distribution
+ * ======================================================================== */
+var sass_files = work_dir+'sass/*.scss',
+    js_files = work_dir+'javascripts/*.js',
+    css_dist = dist_dir+'css/',
+    js_dist = dist_dir+'js/';
+
+/* ========================================================================
+ * Copying tasks
+ * ======================================================================== */
+gulp.task('copy-css-images', function() {
+    return gulp.src(jqueryUiThemeDir+'images/*')
+        .pipe(gulp.dest(css_dist+'images/'));
+});
+gulp.task('copy-fonts', function() {
+    return gulp.src(bs_assets_dir+'fonts/**/*')
+        .pipe(gulp.dest(dist_dir+'fonts/'));
+});
+
+gulp.task('copy',['copy-css-images','copy-fonts']);
+
+/* ========================================================================
+ * Compilation tasks
+ * ======================================================================== */
 gulp.task('styles', function () {
-    console.log('Sassing...');
-    gulp.src(sass_files)
+    return gulp.src(sass_files)
+        .pipe(rename({suffix: '.min'}))
         .pipe(sass({loadPath: bs_styles_dir, style: 'compressed'}))
         .pipe(gulp.dest(css_dist));
 });
 
-gulp.task('scripts', function () {
-    gulp.src(bs_js_deps)
-        .pipe(conc('dependencies.js'))
-        .pipe(gulp.dest(js_dist))
+
+/* ========================================================================
+ * Dependencies concat/minification
+ * ======================================================================== */
+gulp.task('style-deps', function () {
+    return gulp.src(style_deps)
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(concatenate('dependencies.css'))
+        .pipe(minifyCss())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('../css'))
+        .pipe(gulp.dest(css_dist));
+});
+
+gulp.task('script-deps', function () {
+    return gulp.src(js_deps)
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(concatenate('dependencies.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemaps.write('../js'))
         .pipe(gulp.dest(js_dist));
 });
 
+/* ========================================================================
+ * Watch and combined tasks
+ * ======================================================================== */
 gulp.task('watch', function() {
-    console.log('Built-in watch')
     gulp.watch(sass_files, ['styles']);
-    gulp.watch(js_files, ['scripts']);
 });
 
-gulp.task('default',['styles','scripts'])
+gulp.task('all',['copy','styles','style-deps','script-deps']);
+gulp.task('default',['styles','watch']);
