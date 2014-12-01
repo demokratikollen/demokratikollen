621e0cc1f93aef32cdeef66d265c3d004c858850
Joel Goop <joelgoop@gmail.com>
2014-12-01 08:52:23 +0100

    Remove some printing in populate_orm and count errors

--- demokratikollen/populate_orm.py
+++ demokratikollen/populate_orm.py
@@ -93,28 +93,28 @@ for dok_id,rm,bet,organ,publ,titel,dok_url,hangar_id in c:
             committee=committee))
 # s.commit()
 
-pbar = InitBar(title="Adding votes")
-pbar(0)
-polls = {}
-c.execute("SELECT COUNT(*) FROM votering WHERE avser='sakfrågan'")
-num_votes = c.fetchone()[0]
-c_named = source_conn.cursor("named")
-c_named.itersize = 50000
-c_named.execute("SELECT votering_id,intressent_id,beteckning,rm,punkt,rost,datum FROM votering WHERE avser='sakfrågan' ORDER BY votering_id LIMIT 100")
-for i,(votering_id,intressent_id,beteckning,rm,punkt,rost,datum) in enumerate(c_named):
-    if votering_id not in polls:
-        date = datum.date()
-        polls[votering_id] = PolledPoint(poll_date=date,r_votering_id=votering_id,number=punkt)
-        s.add(polls[votering_id])
-    if i % 1000 == 0:
-        add_status = 100*i/num_votes
-        pbar(add_status)
-    if i % 50000 == 0:
-        s.commit()
-    s.add(Vote(member=members[intressent_id],
-                vote_option=rost,polled_point=polls[votering_id]))
+# pbar = InitBar(title="Adding votes")
+# pbar(0)
+# polls = {}
+# c.execute("SELECT COUNT(*) FROM votering WHERE avser='sakfrågan'")
+# num_votes = c.fetchone()[0]
+# c_named = source_conn.cursor("named")
+# c_named.itersize = 50000
+# c_named.execute("SELECT votering_id,intressent_id,beteckning,rm,punkt,rost,datum FROM votering WHERE avser='sakfrågan' ORDER BY votering_id")
+# for i,(votering_id,intressent_id,beteckning,rm,punkt,rost,datum) in enumerate(c_named):
+#     if votering_id not in polls:
+#         date = datum.date()
+#         polls[votering_id] = PolledPoint(poll_date=date,r_votering_id=votering_id,number=punkt)
+#         s.add(polls[votering_id])
+#     if i % 1000 == 0:
+#         add_status = 100*i/num_votes
+#         pbar(add_status)
+#     if i % 50000 == 0:
+#         s.commit()
+#     s.add(Vote(member=members[intressent_id],
+#                 vote_option=rost,polled_point=polls[votering_id]))
 
-del pbar
+# del pbar
 # s.commit()
 
 # Add kammaruppdrag
@@ -171,13 +171,15 @@ for intressent_id,fr,to,roll,abbr,g_name in c:
 print("Adding member proposals.")
 c.execute("""SELECT hangar_id,dok_id,rm,beteckning,publicerad,titel,
                             dokument_url_text,subtyp
-                    FROM dokument WHERE doktyp='mot' AND relaterat_id='' ORDER BY hangar_id LIMIT 1000""")
+                    FROM dokument WHERE doktyp='mot' AND relaterat_id='' ORDER BY hangar_id""")
 m_props = {}
 for hangar_id,dok_id,rm,beteckning,publicerad,titel,dokument_url_text,subtyp in c:
     if subtyp in ['Enskild motion','Kommittémotion','Följdmotion','Partimotion','Flerpartimotion','Fristående motion']:
         subtype = subtyp
     else:
         subtype = '-'
+    if hangar_id in m_props:
+        raise Exception('Member proposal with hangar_id {} already exists.'.format(hangar_id))
     m_props[hangar_id] = MemberProposal(
                             dok_id=dok_id,
                             published=publicerad,
@@ -191,7 +193,7 @@ for hangar_id,dok_id,rm,beteckning,publicerad,titel,dokument_url_text,subtyp in 
 print("Adding signatories of member proposals.")
 c.execute("""SELECT d.hangar_id,i.intressent_id FROM dokument AS d 
                 JOIN dokintressent AS i ON i.hangar_id=d.hangar_id 
-                WHERE d.doktyp='mot' ORDER BY d.hangar_id LIMIT 1000""")
+                WHERE d.doktyp='mot' ORDER BY d.hangar_id""")
 for hangar_id,intressent_id in c:
     m_props[hangar_id].signatories.append(members[intressent_id])
 
@@ -200,7 +202,9 @@ print("Adding points of member proposals.")
 c.execute("""SELECT d.hangar_id,f.utskottet,f.nummer,f.kammaren,f.behandlas_i 
                 FROM dokument AS d 
                 JOIN dokforslag AS f ON f.hangar_id=d.hangar_id 
-                WHERE d.doktyp='mot' ORDER BY d.hangar_id LIMIT 10""")
+                WHERE d.doktyp='mot' ORDER BY d.hangar_id""")
+m_missing_crs = 0
+m_props_tot = 0
 for hangar_id,utskottet,nummer,kammaren,behandlas_i in c:
     decision_options = ['Avslag','Bifall','Delvis bifall']
     utskottet = utskottet.strip()
@@ -214,10 +218,10 @@ for hangar_id,utskottet,nummer,kammaren,behandlas_i in c:
     if utskottet in decision_options:
         com_recommendation = utskottet
     else:
-        print(utskottet)
         com_recommendation = '-'
 
     try:
+        m_props_tot += 1
         rm,bet = behandlas_i.split(':')
         cr = s.query(CommitteeReport).filter_by(session=rm,code=bet).one()
         p = ProposalPoint(
@@ -226,8 +230,8 @@ for hangar_id,utskottet,nummer,kammaren,behandlas_i in c:
                 committee_recommendation=com_recommendation,
                 decision=ch_decision,
                 committee_report=cr)
-    except ValueError as e:
-        print("{} cannot be split.".format(behandlas_i))
+    except (ValueError,NoResultFound) as e:
+        m_missing_crs += 1
         p = ProposalPoint(
                 proposal=m_props[hangar_id],
                 number=nummer,
@@ -235,20 +239,25 @@ for hangar_id,utskottet,nummer,kammaren,behandlas_i in c:
                 decision=ch_decision)
 
     s.add(p)
-
+print("{} of {} member proposal points missing committee report.".format(m_missing_crs,m_props_tot))
 
 print("Adding government proposals.")
 c.execute("""SELECT hangar_id,dok_id,rm,beteckning,publicerad,titel,
                             dokument_url_text,organ
                     FROM dokument WHERE doktyp='prop' AND relaterat_id='' AND subtyp='prop'
-                    ORDER BY hangar_id LIMIT 1000""")
+                    ORDER BY hangar_id""")
 g_props = {}
+g_missing_ministry = 0
+g_tot = 0
 for hangar_id,dok_id,rm,beteckning,publicerad,titel,dokument_url_text,organ in c:
     if subtyp in ['Enskild motion','Kommittémotion','Följdmotion','Partimotion','Flerpartimotion','Fristående motion']:
         subtype = subtyp
     else:
         subtype = '-'
+    if hangar_id in g_props:
+        raise Exception('Government proposal with hangar_id {} already exists.'.format(hangar_id))
     try:
+        g_tot += 1
         m = s.query(Ministry).filter(func.lower(Ministry.abbr)==organ.lower()).one()
         g_props[hangar_id] = GovernmentProposal(
                                 dok_id=dok_id,
@@ -259,7 +268,7 @@ for hangar_id,dok_id,rm,beteckning,publicerad,titel,dokument_url_text,organ in c
                                 text_url=dokument_url_text,
                                 ministry=m)
     except NoResultFound as e:
-        print("{} not found.".format(organ))
+        g_missing_ministry += 1
         g_props[hangar_id] = GovernmentProposal(
                                 dok_id=dok_id,
                                 published=publicerad,
@@ -268,12 +277,17 @@ for hangar_id,dok_id,rm,beteckning,publicerad,titel,dokument_url_text,organ in c
                                 title=titel,
                                 text_url=dokument_url_text)
     s.add(g_props[hangar_id])
+print("{} of {} government proposals missing ministry.".format(g_missing_ministry,g_tot))
+
 
 print("Adding points of government proposals.")
 c.execute("""SELECT d.hangar_id,f.utskottet,f.nummer,f.kammaren,f.behandlas_i 
                 FROM dokument AS d 
                 JOIN dokforslag AS f ON f.hangar_id=d.hangar_id 
-                WHERE d.doktyp='prop' AND d.subtyp='prop' ORDER BY d.hangar_id LIMIT 10""")
+                WHERE d.doktyp='prop' AND d.subtyp='prop' ORDER BY d.hangar_id""")
+g_props_tot = 0
+g_prop_missing = 0
+g_missing_crs = 0
 for hangar_id,utskottet,nummer,kammaren,behandlas_i in c:
     decision_options = ['Avslag','Bifall','Delvis bifall']
     utskottet = utskottet.strip()
@@ -287,10 +301,14 @@ for hangar_id,utskottet,nummer,kammaren,behandlas_i in c:
     if utskottet in decision_options:
         com_recommendation = utskottet
     else:
-        print(utskottet)
         com_recommendation = '-'
 
+    if hangar_id not in g_props:
+        g_prop_missing += 1
+        continue
+
     try:
+        g_props_tot += 1
         rm,bet = behandlas_i.split(':')
         cr = s.query(CommitteeReport).filter_by(session=rm,code=bet).one()
         p = ProposalPoint(
@@ -299,8 +317,8 @@ for hangar_id,utskottet,nummer,kammaren,behandlas_i in c:
                 committee_recommendation=com_recommendation,
                 decision=ch_decision,
                 committee_report=cr)
-    except ValueError as e:
-        print("{} cannot be split.".format(behandlas_i))
+    except (ValueError,NoResultFound) as e:
+        g_missing_crs += 1
         p = ProposalPoint(
                 proposal=g_props[hangar_id],
                 number=nummer,
@@ -308,7 +326,8 @@ for hangar_id,utskottet,nummer,kammaren,behandlas_i in c:
                 decision=ch_decision)
 
     s.add(p)
-
+print("{} government proposal points missing proposal document.".format(g_prop_missing))
+print("{} of {} government proposal points missing committee report.".format(g_missing_crs,g_props_tot))
 
 print("Adding committee report points")
 c.execute("""SELECT rm,bet,punkt,rubrik,beslutstyp,votering_id FROM dokutskottsforslag""")
