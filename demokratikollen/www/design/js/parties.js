23d840ad46a458099fa91c68f2c1b2e724b3c049
Joel Goop <joelgoop@gmail.com>
2014-12-10 22:22:28 +0100

    Cleanup JavaScript for map plot and bind data by id

--- demokratikollen/www/design/js/parties.js
+++ demokratikollen/www/design/js/parties.js
@@ -1,51 +1,80 @@
 parties = {
-  setup: function() {
-    d3.json('/data/municipalities.topojson', function (error,data) {
-      if (error) return console.error(error);
-      // console.log(data);
+  mapChart: function(regions) {
+    var width = 130,
+        height = 2.53846153846*width;
 
-      var municipalities = topojson.feature(data, data.objects.municipalities);
-      // var projection = d3.geo.mercator();
-      var width = 130,
-          height = 330;
+    var transform = 'rotate(10,'+width/2+','+height/2+')';
 
-       var svg = d3.select("#election-map")
-            .attr('width',width)
-            .attr('height',height);
+    var projection = d3.geo.conicEqualArea()
+        .parallels([50,70])
+        .center([16.6358,62.1792])        
 
-      var projection = d3.geo.conicEqualArea()
-          .parallels([50,70])
-          .scale(1300)
-          .center([16.6358,62.1792])
-          .translate([width / 2, height / 2]);
-      var path = d3.geo.path()
-          .projection(projection);
+    var path = d3.geo.path();
 
-      svg.append("rect")
-          .attr("width", "100%")
-          .attr("height", "100%")
-          .attr("fill", "pink");
+    function idProp(d) {
+      return d.id;
+    }
 
-      svg.append("g")
-          .attr('transform','rotate(10,'+width/2+','+height/2+')')
-        .selectAll(".municipality")
-          .data(municipalities.features)
-        .enter()
-          .append("path")
-          .attr("class","municipality")
-          .attr("d", path)
-          .attr("fill", function(d){
-            var num = parseInt(d.id),
-                col = d3.hsl(num/2584*360,1,0.5);
-            return col.rgb();
-          });
+    function chart(selection) {
+      console.log(regions);
+      selection.each(function(data, i) {
+        var svg = d3.select(this).selectAll("svg").data([data]);
 
-      var point = projection([14.6358,63.1792]);
-      svg.append("circle")
-          .attr("cx",point[0])
-          .attr("cy",point[1])
-          .attr("r",3)
-          .attr("fill", "blue");
+        console.log(svg);
+
+        // Update projection and path
+        projection.scale(10*width)
+            .translate([width / 2, height / 2]);
+        path.projection(projection);
+
+        // Otherwise, create the skeletal chart.
+        var gMain = svg.enter().append("svg").append("g");
+        var sel = gMain.attr("class","map-collection")
+            .attr('transform',transform)
+            .selectAll(".map-region")
+            .data(regions.features, idProp)
+            .enter()
+            .append("path")
+            .attr("class","map-region")
+            .attr("d", path);
+
+        console.log(sel);
+
+        svg.attr("width", width)
+            .attr("height", height)
+            .selectAll(".map-region")
+            .data(data, idProp)
+            .attr("fill",function(d) { return d.color });
+      });
+    }
+
+    chart.projection = function(_) {
+      if (!arguments.length) return projection;
+      projection = _;
+      return chart;
+    };
+
+    chart.width = function(_) {
+      if (!arguments.length) return width;
+      width = _;
+      return chart;
+    };
+
+    chart.height = function(_) {
+      if (!arguments.length) return height;
+      height = _;
+      return chart;
+    };
+
+    return chart;
+  },
+  setup: function(divId,data) {
+    d3.json('/data/municipalities.topojson', function (error,json) {
+      var municipalities = topojson.feature(json, json.objects.municipalities);
+      var mapChart = parties.mapChart(municipalities);
+      d3.select(divId)
+        .datum(data)
+        .call(mapChart);
     });
   }
 };
