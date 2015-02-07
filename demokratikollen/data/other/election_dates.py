e0645db58cd90456ee0784fba1eb16ca4dc0a443
Rasmus Einarsson <rasmus.einarsson@gmail.com>
2015-02-07 16:01:56 +0100

    Put election dates in separate file

---
+++ demokratikollen/data/other/election_dates.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+import datetime as dt
+
+# From se.wikipedia.org...
+
+ELECTION_DATES = {
+    1973: dt.datetime(1973, 9, 16),
+    1976: dt.datetime(1976, 9, 19),
+    1979: dt.datetime(1979, 9, 16),
+    1982: dt.datetime(1982, 9, 19),
+    1985: dt.datetime(1985, 9, 15),
+    1988: dt.datetime(1988, 9, 18),
+    1991: dt.datetime(1991, 9, 15),
+    1994: dt.datetime(1994, 9, 18),
+    1998: dt.datetime(1998, 9, 20),
+    2002: dt.datetime(2002, 9, 15),
+    2006: dt.datetime(2006, 9, 17),
+    2010: dt.datetime(2010, 9, 19),
+    2014: dt.datetime(2014, 9, 14),
+    2018: dt.datetime(2018, 9, 9)    
+}
