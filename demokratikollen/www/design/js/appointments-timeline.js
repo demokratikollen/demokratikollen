842734ce210fdee90ca070985ffb9c35f834c7eb
Rasmus Einarsson <rasmus.einarsson@gmail.com>
2015-02-08 23:03:08 +0100

    Fix #71
    Harmonize design of appointments figure with other figs.
    Get rid of d3-tip.

--- demokratikollen/www/design/js/appointments-timeline.js
+++ demokratikollen/www/design/js/appointments-timeline.js
@@ -1,8 +1,10 @@
-demokratikollen.graphics.AppointmentsTimeline = function() {
+/*global demokratikollen, d3, window */
+
+demokratikollen.graphics.AppointmentsTimeline = function () {
 
   var rowHeight = 40,
     lineOffsetY = 10,
-    margin = { top: 40, right: 0, bottom: 5, left: 0 },
+    margin = { top: 20, right: 0, bottom: 5, left: 0 },
     rowLabelsMarginLeft = 10,
     timeUnit = d3.time.year,
     tickLabelWidth = 100,
@@ -11,50 +13,51 @@ demokratikollen.graphics.AppointmentsTimeline = function() {
     cssClass = "timeline",
     hoverLinesWidth = rowHeight - 15;
 
-  function getMinDate(blocks) { 
+  function getMinDate(blocks) {
     return d3.min(blocks, function (block) { return block.startDate; });
   }
 
-  function getMaxDate(blocks) { 
+  function getMaxDate(blocks) {
     return d3.max(blocks, function (block) { return block.endDate; });
   }
 
   function ordering(rowA, rowB) {
     // rowA and rowB are arrays of data entries
-    maxDates = [getMaxDate(rowA.values), getMaxDate(rowB.values)];
-    if (maxDates[0] != maxDates[1]) {
+    var maxDates = [getMaxDate(rowA.values), getMaxDate(rowB.values)];
+    if (maxDates[0] !== maxDates[1]) {
       return maxDates[1] - maxDates[0];
-    } else {
-      return getMinDate(rowB.values) - getMinDate(rowA.values);
     }
+    return getMinDate(rowB.values) - getMinDate(rowA.values);
 
   }
 
   function unique(arr) {
-    return arr.reduce(function(collected, current) {
-      if (collected.indexOf(current) < 0) collected.push(current);
+    return arr.reduce(function (collected, current) {
+      if (collected.indexOf(current) < 0) {
+        collected.push(current);
+      }
       return collected;
     }, []);
-  };
+  }
 
   function chart(selection) {
-    selection.each(function(data) {
+    selection.each(function (data) {
 
-      dataByRowLabel = d3.nest()
-        .key(function(d) { return d.rowLabel; })
+      var dataByRowLabel = d3.nest()
+        .key(function (d) { return d.rowLabel; })
         .entries(data);
       dataByRowLabel.sort(ordering);
 
 
-      var rowLabels = dataByRowLabel.map(function(d) { return d.key; }),
+      var rowLabels = dataByRowLabel.map(function (d) { return d.key; }),
         minDate = getMinDate(data),
         maxDate = getMaxDate(data);
 
-      
-      var width = this.getBoundingClientRect().width - margin.left - margin.right,
-          height = rowLabels.length * rowHeight;
+      var width = demokratikollen.utils.getInnerWidth(d3.select(this)) - margin.left - margin.right,
+        height = rowLabels.length * rowHeight;
 
-      var svg = d3.select(this)
+      var container = d3.select(this);
+      var svg = container
         .append("svg")
         .classed(cssClass, true)
         .attr("width", width + margin.left + margin.right)
@@ -77,7 +80,6 @@ demokratikollen.graphics.AppointmentsTimeline = function() {
         .rangePoints([0, height], 1);
 
       var tickValues = x.ticks(Math.floor(width / tickLabelWidth));
-
       tickValues = unique(tickValues.map(timeUnit.floor));
 
       var xAxis = d3.svg.axis()
@@ -91,113 +93,123 @@ demokratikollen.graphics.AppointmentsTimeline = function() {
         .call(xAxis);
       xAxis.selectAll("text")
         .style("text-anchor", "start");
-      
+
       svg.append("g")
         .classed("rowLabels", true)
         .selectAll("rowLabels")
         .data(rowLabels)
         .enter()
         .append("text")
-        .text(function(d) { return d; })
-        .attr("transform", function(d) { return "translate(" + rowLabelsMarginLeft +"," + y(d) + ")"; });
+        .text(function (d) { return d; })
+        .attr("transform", function (d) { return "translate(" + rowLabelsMarginLeft + "," + y(d) + ")"; });
 
       var line = d3.svg.line()
-        .x(function(d) { return x(d[0]); })
-        .y(function(d) { return y(d[1]); });
+        .x(function (d) { return x(d[0]); })
+        .y(function (d) { return y(d[1]); });
 
-      linesSelection = svg.append("g")
-        .attr("transform", function(d) { return "translate(0," + lineOffsetY + ")"; })
+      var linesSelection = svg.append("g")
+        .attr("transform", "translate(0," + lineOffsetY + ")")
         .classed("lines", true)
         .selectAll("lines")
         .data(data)
         .enter();
 
-      lines = linesSelection.append("path")
-        .attr("d", function(d) { 
-          return line([[d.startDate, d.rowLabel], [d.endDate, d.rowLabel]]); 
+      linesSelection.append("path")
+        .attr("d", function (d) {
+          return line([[d.startDate, d.rowLabel], [d.endDate, d.rowLabel]]);
         })
-        .attr("class", function(d) { return d.cssClass || ""; });
+        .attr("class", function (d) { return d.cssClass || ""; });
 
       linesSelection.append("circle")
-        .attr("cx", function(d) { return x(d.startDate); })
-        .attr("cy", function(d) { return y(d.rowLabel); })
+        .attr("cx", function (d) { return x(d.startDate); })
+        .attr("cy", function (d) { return y(d.rowLabel); })
         .attr('r', markerSize);
 
       linesSelection.append("circle")
-        .attr("cx", function(d) { return x(d.endDate); })
-        .attr("cy", function(d) { return y(d.rowLabel); })
+        .attr("cx", function (d) { return x(d.endDate); })
+        .attr("cy", function (d) { return y(d.rowLabel); })
         .attr('r', markerSize);
 
 
       if (tipHtml) {
-        tip = d3.tip()
-        .attr('class', 'd3-tip timeline')
-        .direction(function(d) {
-          var startX = x(d.startDate),
-            endX = x(d.endDate),
-            lineMidX = (startX + endX) / 2,
-            figMidX = width/2;
+        var tooltip = d3.select("body")
+          .append("div")
+          .classed(cssClass + " tooltip", true)
+          .style("display", "hidden")
+          .style("position", "absolute")
+          .style("max-width", width + "px");
 
-          if (startX <= figMidX && figMidX <= endX) { 
-            // Line covers middle of figure.
-            return 's';
-          } else if (startX >= figMidX) { 
-            // Line is right of middle of figure.
-            return 'sw';
-          } else if (endX <= figMidX) { 
-            // Line is left of middle of figure.
-            return 'se';
-          }
-        })
-        .offset(function(d) {
-          var startX = x(d.startDate),
-            endX = x(d.endDate),
-            lineMidX = (startX + endX) / 2,
-            figMidX = width/2;
+        var setPosTooltip = function (x, y) {
+          tooltip.style("left", x + "px")
+            .style("top", y + "px");
+        };
 
-          if (startX <= figMidX && figMidX <= endX) { 
-            // Line covers middle of figure. Move tooltip to fig middle.
-            return [hoverLinesWidth/2+1, figMidX-lineMidX]; 
-          } else if (startX >= figMidX) { 
-            // Line is right of middle of figure.
-            return [hoverLinesWidth/2+1, -hoverLinesWidth/3];
-          } else if (endX <= figMidX) { 
-            // Line is left of middle of figure.
-            return [hoverLinesWidth/2+1, hoverLinesWidth/3];
+        var hideTooltip = function () {
+          tooltip.style("display", "none");
+        };
+
+        var showTooltip = function (d) {
+          tooltip.style("display", "block");
+          tooltip.html(tipHtml(d));
+          setPosTooltip(0, 0);
+          /*console.log(tooltip[0][0].getBoundingClientRect());
+          return;*/
+
+          var mouseEvent = d3.mouse(d3.select("body")[0][0]),
+            anchorX = mouseEvent[0],
+            anchorY = mouseEvent[1],
+            tooltipBCR = tooltip[0][0].getBoundingClientRect(),
+            tooltipWidth = tooltipBCR.right - tooltipBCR.left,
+            plotAreaBCR = svg[0][0].getBoundingClientRect(),
+            edge = {
+              left: window.pageXOffset + plotAreaBCR.left,
+              right: window.pageXOffset + plotAreaBCR.left + width
+            };
+
+          if (anchorX + tooltipWidth / 2 > edge.right) {
+            anchorX = edge.right - tooltipWidth / 2;
+          } else if (anchorX - tooltipWidth / 2 < edge.left) {
+            anchorX = edge.left + tooltipWidth / 2;
           }
-        })
-        .html(tipHtml);
+
+          setPosTooltip(anchorX - tooltipWidth / 2, anchorY + rowHeight / 2);
+        };
+
+
 
         linesSelection.append("path")
           .style("stroke", "rgba(0,0,0,0)")
           .style("stroke-width", hoverLinesWidth)
-          .attr("d", function(d) { return line([[d.startDate, d.rowLabel], [d.endDate, d.rowLabel]]); })
-          .call(tip)
-          .on('mouseover', tip.show)
-          .on('mouseout', tip.hide);
+          .attr("d", function (d) { return line([[d.startDate, d.rowLabel], [d.endDate, d.rowLabel]]); })
+          .on('mouseover', showTooltip)
+          .on('mouseout', function (d) { hideTooltip(); });
       }
-        
-
     });
   }
 
-  chart.tipHtml = function(value) {
-    if (!arguments.length) return tipHtml;
+  chart.tipHtml = function (value) {
+    if (!arguments.length) {
+      return tipHtml;
+    }
     tipHtml = value;
     return chart;
   };
 
-  chart.timeUnit = function(value) {
-    if (!arguments.length) return timeUnit;
+  chart.timeUnit = function (value) {
+    if (!arguments.length) {
+      return timeUnit;
+    }
     timeUnit = value;
     return chart;
   };
 
-  chart.tickLabelWidth = function(value) {
-    if (!arguments.length) return tickLabelWidth;
+  chart.tickLabelWidth = function (value) {
+    if (!arguments.length) {
+      return tickLabelWidth;
+    }
     tickLabelWidth = value;
     return chart;
   };
 
   return chart;
-}
+};
