681bab82ad9383316cee7695628b66be4cd26880
Johan Rohlen <johan@rohlen.net>
2017-06-14 14:57:13 +0100

    Add behave tests to travis build.
    "Fix" behave tests to work with travis. Mainly change to use 8000 all
    the time. Don't remember why we used 5000, but let's stop using that.
    
    Added a debug on failure flag for the behave tests to help with
    debugging weird issues.

--- demokratikollen/www/features/environment.py
+++ demokratikollen/www/features/environment.py
@@ -4,13 +4,9 @@ import os
 import shlex
 import time
 
-
 def before_all(context):
     print("Starting Webserver and Webdriver")
-    if 'DEPLOY' in os.environ:
-    	context.server = subprocess.Popen(shlex.split("gunicorn --debug -b 127.0.0.1:5000 demokratikollen.www.gunicorn_no_cache:app"),stdout=subprocess.PIPE)
-    else:
-       	context.server = subprocess.Popen(shlex.split("gunicorn --debug -b 127.0.0.1:8000 demokratikollen.www.gunicorn_no_cache:app"),stdout=subprocess.PIPE)
+    context.server = subprocess.Popen(shlex.split("gunicorn --debug -b 127.0.0.1:8000 demokratikollen.www.gunicorn_no_cache:app"),stdout=subprocess.PIPE)
     context.driver = webdriver.PhantomJS()
 
 def after_all(context):
@@ -20,3 +16,10 @@ def after_all(context):
     out,err = context.server.communicate()
     print("Output from the webserver:")
     print(out)
+
+def after_step(context, step):
+    if 'BEHAVE_DEBUG_ON_ERROR' in os.environ and step.status == "failed":
+        # -- ENTER DEBUGGER: Zoom in on failure location.
+        # NOTE: Use IPython debugger, same for pdb (basic python debugger).
+        import ipdb
+        ipdb.post_mortem(step.exc_traceback)
