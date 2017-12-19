11b4aa4035bc00feff0705f66a23890f054641b9
Johan Rohlen <johan@rohlen.net>
2017-12-19 21:14:25 +0100

    First go at a deploy step.

---
+++ ci/deploy.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+ssh_command="ssh -i ci/deploy_key -l demokratikollen $SERVER_TO_DEPLOY_TO"
+
+$ssh_command rm -rf demokratikollen
+$ssh_command git clone --shallow --branch ${TRAVIS_BRANCH} https://github.com/demokratikollen/demokratikollen.git
+$ssh_command cd demokratikollen/docker/prod && BRANCH=${TRAVIS_BRANCH} docker-compose up -d
