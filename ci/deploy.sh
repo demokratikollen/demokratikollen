23d5525daa3039c5ba825d7ac07d6680c02ccef8
Johan Rohlén <johan@rohlen.net>
2019-02-03 09:42:32 +0100

    Fix path to environment and namespace deploy type

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -5,10 +5,11 @@ declare TRAVIS_COMMIT
 
 ENVIRONMENT=${1:-staging}
 
-source environment.sh
+source ci/environment.sh
 
 ssh_command="ssh -o StrictHostKeyChecking=no -o BatchMode=yes -i ci/deploy_key -l demokratikollen $SERVER_TO_DEPLOY_TO"
 
-${ssh_command} rm -rf demokratikollen
-${ssh_command} git clone https://github.com/demokratikollen/demokratikollen.git
-${ssh_command} cd demokratikollen/docker/compose/${ENVIRONMENT} && postgres_md5=${POSTGRES_DATA_HASH} mongodb_md5=${MONGO_DATA_HASH} git_sha=${TRAVIS_COMMIT} docker-compose up -d
+${ssh_command} mkdir -p ${ENVIRONMENT}
+${ssh_command} rm -rf ${ENVIRONMENT}/demokratikollen
+${ssh_command} cd ${ENVIRONMENT} && git clone https://github.com/demokratikollen/demokratikollen.git
+${ssh_command} cd ${ENVIRONMENT}/demokratikollen/docker/compose/${ENVIRONMENT} && postgres_md5=${POSTGRES_DATA_HASH} mongodb_md5=${MONGO_DATA_HASH} git_sha=${TRAVIS_COMMIT} docker-compose up -d
