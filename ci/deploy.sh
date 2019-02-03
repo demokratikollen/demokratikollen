1cc7c892e589c2a255caab8c101a410273179351
Johan Rohlén <johan@rohlen.net>
2019-02-03 18:43:12 +0100

    escaping of ssh commands (really, time for ansible)

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -14,5 +14,6 @@ ssh_command="ssh -o StrictHostKeyChecking=no -o BatchMode=yes -i ci/deploy_key -
 
 ${ssh_command} mkdir -p ${ENVIRONMENT}
 ${ssh_command} rm -rf ${ENVIRONMENT}/demokratikollen
-${ssh_command} cd ${ENVIRONMENT} && git clone https://github.com/demokratikollen/demokratikollen.git
-${ssh_command} cd ${ENVIRONMENT}/demokratikollen/docker/compose/${ENVIRONMENT} && postgres_md5=${POSTGRES_DATA_HASH} mongodb_md5=${MONGO_DATA_HASH} git_sha=${TRAVIS_COMMIT} docker-compose up -d
+${ssh_command} "cd ${ENVIRONMENT} && git clone https://github.com/demokratikollen/demokratikollen.git"
+${ssh_command} "cd ${ENVIRONMENT}/demokratikollen/docker/compose/${ENVIRONMENT} && postgres_md5=${POSTGRES_DATA_HASH} mongodb_md5=${MONGO_DATA_HASH} git_sha=${TRAVIS_COMMIT} /opt/bin/docker-compose pull"
+${ssh_command} "cd ${ENVIRONMENT}/demokratikollen/docker/compose/${ENVIRONMENT} && postgres_md5=${POSTGRES_DATA_HASH} mongodb_md5=${MONGO_DATA_HASH} git_sha=${TRAVIS_COMMIT} /opt/bin/docker-compose up -d"
